af3783b6567c4326c556e9e64948c48aba248ff7
NripkeSchool <89931379+NripkeSchool@users.noreply.github.com>
2023-04-27 11:30:11 -0400

    Finish CatGame.java

--- CatGame.java
+++ CatGame.java
@@ -4,82 +4,96 @@ public class CatGame
 {
     private EdgeWeightedGraph board;
     private int n;
+    private boolean[][] marked;
+    private int catPosition;
+    private int FREEDOM;
     public CatGame(int n)
     {
         this.n = n;
         board = new EdgeWeightedGraph(n*n+1);
+        marked = new boolean[n][n];
+        catPosition = (n/2)*(n/2);
+        FREEDOM = n*n + 1;
 
         //Do the inside edges first
         for (int row = 1; row<n-1; row++)
         {
             for (int col = 1; col<n-1; col++)
             {   
-                int v = n*row + col; //Current vertex
+                int v = getVertex(row, col); //Current vertex
 
                 //Add all 6 edges
-                board.addEdge(new Edge(v, v-n, 1));
-                board.addEdge(new Edge(v, v-n+1, 1));
+                board.addEdge(new CatEdge(v, getVertex(row-1, col+1)));
+                board.addEdge(new CatEdge(v, getVertex(row-1, col-1)));
 
-                board.addEdge(new Edge(v, v-1, 1));
-                board.addEdge(new Edge(v, v+1, 1));
+                board.addEdge(new CatEdge(v, getVertex(row, col+1)));
+                board.addEdge(new CatEdge(v, getVertex(row, col-1)));
 
-                board.addEdge(new Edge(v, v+n, 1));
-                board.addEdge(new Edge(v, v+n+1, 1));
+                board.addEdge(new CatEdge(v, getVertex(row+1, col+1)));
+                board.addEdge(new CatEdge(v, getVertex(row+1, col-1)));
             }
         }
 
-        //Do outside edges
+        //Do outside edge
         for (int col = 0; col<n-1; col++)
         {
-            board.addEdge(new Edge(col, col+1, 1));
-            board.addEdge(new Edge(col, n*n+1, 0));
-        }
-        board.addEdge(new Edge(n-1, n*n+1, 0));
+            board.addEdge(new CatEdge(getVertex(0, col), getVertex(0, col+1)));
+            board.addEdge(new CatEdge(getVertex(0, col), FREEDOM));
 
-        for (int col = 0; col<n-1; col++)
-        {
-            board.addEdge(new Edge(col + n*(n-1), col + n*(n-1) + 1, 1));
-            board.addEdge(new Edge(col + n*(n-1), n*n+1, 0));
+            board.addEdge(new CatEdge(getVertex(n-1, col), getVertex(n-1, col+1)));
+            board.addEdge(new CatEdge(getVertex(n-1, col), FREEDOM));
         }
-        board.addEdge(new Edge(n*n, n*n+1, 0));
+
+        board.addEdge(new CatEdge(getVertex(0, n-1), FREEDOM));
+        board.addEdge(new CatEdge(getVertex(n-1, n-1), FREEDOM));
 
         for (int row = 0; row<n-1; row++)
         {
-            board.addEdge(new Edge(row*n, row*n+n, 1));
-            board.addEdge(new Edge(row*n, n*n+1, 0));
+            board.addEdge(new CatEdge(getVertex(row, 0), getVertex(row+1, 0)));
+            board.addEdge(new CatEdge(getVertex(row, 0), FREEDOM));
+
+            board.addEdge(new CatEdge(getVertex(row, n-1), getVertex(row+1, n-1)));
+            board.addEdge(new CatEdge(getVertex(row, n-1), FREEDOM));
         }
 
-        for (int row = 0; row<n-1; row++)
-        {
-            board.addEdge(new Edge(getVertex(row, n-1), getVertex(row+1, n-1), 1));
-            board.addEdge(new Edge(getVertex(row, n-1), n*n+1, 0));
-        }
+        board.addEdge(new CatEdge(getVertex(0, 0), FREEDOM));
+        board.addEdge(new CatEdge(getVertex(n-1, 0), FREEDOM));
     }
 
     private int getVertex(int row, int col) {return row*n + col;}
 
     public void markTile(int row, int col)
     {
+        DjikstraUndirectedSP shortestPath = new DjikstraUndirectedSP(board, catPosition);
+        CatEdge e = (CatEdge) shortestPath.pathTo(FREEDOM).iterator().next();
+        catPosition = e.other(catPosition);
 
+        for (Edge ce : adj[getVertex(row, col)])
+        {
+            ce = (CatEdge) ce;
+            ce.markEdge();
+        }
+        marked[row][col] = true;
     }
 
     public boolean marked(int row, int col)
     {
-
+        return marked[row][col];
     }
 
     public int[] getCatTile() //Return next cat move
     {
-
+        return new int[]{catPosition/n, catPosition % n};
     }
 
     public boolean catHasEscaped()
     {
-
+        return catPosition == FREEDOM;
     }
 
     public boolean catIsTrapped()
     {
-
+        DjikstraUndirectedSP shortestPath = new DjikstraUndirectedSP(board, catPosition);
+        return shortestPath.distTo[FREEDOM] == Double.POSITIVE_INFINITY;
     }
 }
